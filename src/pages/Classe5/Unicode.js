a24101d23d6c4560e917050d7e6d00fbef042f96
Andrea <andrea.lin3110@gmail.com>
2021-02-18 19:11:18 +0100

    ⛓ Added Unicode's Page content.

--- src/pages/Classe5/Unicode.js
+++ src/pages/Classe5/Unicode.js
@@ -11,12 +11,34 @@ export default function Unicode() {
             <Section>
                 <div className="columns">
                     <div className="column">
-                        <p>
-                            <small>
-                                9 aprile 2019 - Classe 4<sup>a</sup>D.
-                            </small>
+                        <p>13, 27 novembre 2020 - Andrea Lin, Mattia Ferrari.</p>
+                        <br />
+                        <h1 className="title is-size-4">Richiesta</h1>
+                        <p className="subtitle is-size-6">
+                            Scegliere uno dei tre progetti mostrati e presentare delle slide PowerPoint per punti con descrizione sintetica dell’approccio utilizzato e descrizione dettagliata degli output di progetto richiesti
                         </p>
-                        todo
+                        <hr />
+                        <h1 className="title is-size-4">Cosa abbiamo fatto?</h1>
+                        <p className="subtitle is-size-6">
+                            Noi abbiamo scelto di presentare il progetto numero 3: «Soluzione per Comandi Gestuali» perché secondo noi era il progetto più interessante dei 3 proposti.
+                            <br />È possibile visualizzare la presentazione da noi realizzata con il pulsante sottostante.
+                        </p>
+                        <hr />
+                        <h1 className="title is-size-4">Conclusioni</h1>
+                        <p className="subtitle is-size-6">L'attività è stata molto interessante, ma sono rimasto un po' deluso perché mi aspettavo qualcosa di più pratico.</p>
+                        <hr />
+                        <div className="columns">
+                            <div className="column">
+                                <a className="button is-link is-outlined is-fullwidth is-hoverable" rel="noopener noreferrer" target="_blank" href="https://linandrea.netlify.app/unicode/Progetto3_Soluzione_per_Comandi_Gestuali.pdf">
+                                    Presentazione
+                                </a>
+                            </div>
+                            <div className="column">
+                                <a className="button is-success is-outlined is-fullwidth is-hoverable" rel="noopener noreferrer" target="_blank" href="https://linandrea.netlify.app/unicode/Presentazione_e_consegna_UNICODE.pdf">
+                                    Azienda e consegne
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Section>
